8449277b7b84054aebec26a67cfe269e83e69c21
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-23 16:15:27 +0500

    Create problem20.py

---
+++ problem20.py
@@ -0,0 +1,10 @@
+#Python 3.7.4
+#https://projecteuler.net/problem=20
+"""
+There is a deliberate error in the code. 
+Do you understand Python for a long time to find her.
+"""
+import math
+num = 100
+res = factorial(num)
+print (sum(map(int, str(res))))
