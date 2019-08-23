70f0ba08243ae9e4426d58ad73046c8687a1b23d
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-23 22:27:05 +0500

    Create problem7.py

---
+++ problem7.py
@@ -0,0 +1,8 @@
+#Python 3.7.4
+#https://projecteuler.net/problem=7
+"""
+There is a deliberate error in the code. 
+Do you understand Python for a long time to find her
+"""
+from my_function import primes_list_bw
+print ('Result of the problem 7:', primes_list_bw(10001)[10001])
