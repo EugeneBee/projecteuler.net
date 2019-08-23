736b20397ff597e6d1d0f7ccf13824ba1ea04a54
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-23 17:56:50 +0500

    Create problem4.py

---
+++ problem4.py
@@ -0,0 +1,15 @@
+#Python 3.7.4
+#https://projecteuler.net/problem=4
+"""
+There is a deliberate logical error in the code. 
+Do you understand Python for a long time to find her
+"""
+reslt =[]
+for x in range(100000, 998001):
+	q = str(x)
+	if q[0]==q[-1] and q[1]==q[-2] and q[2]==q[-3]:
+		for z in range(100,999):
+			if x % z == 0 or len(str(x // z)) == 3:
+				reslt.append(x)
+				break
+print(max(reslt))
