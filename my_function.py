9702338481974a764ecc2f4f5513a7cd6d024fb8
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-23 17:32:30 +0500

    Create my_function.py

---
+++ my_function.py
@@ -0,0 +1,29 @@
+#Python 3.7.4
+
+def primes_list_bw(num):
+    """returns a list of primes to the accepted number"""
+    #import code:
+    # from my_function import primes_list_bw
+    if num < 2:
+        return []
+    ln = (num//2) - 1
+    prnum = [True]*ln  
+    for i in range(int(ln**0.5)):  
+        if prnum[i]:
+            for j in range(2*i*(i + 3) + 3, ln, 2*i + 3):
+                prnum[j] = False  
+    return [2] + [i*2 + 3 for i, j in enumerate(prnum) if j]
+
+def sum_divis(num):
+    """
+    returns the sum of the divisors of the natural argument num
+    """
+    #import code:
+    # from my_function import sum_divis
+    ms = [1]
+    for i in range(2,int(num**0.5)+1):
+        if num % i == 0 and i not in ms:
+            ms.append(i)
+            if int(num/i) not in ms:
+                ms.append(int(num/i))
+    return ms
