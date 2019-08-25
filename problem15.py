f1786db2af7af07ca768b10b79457ea2290e5aba
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-25 13:17:54 +0500

    Create problem15.py

---
+++ problem15.py
@@ -0,0 +1,8 @@
+#Python 3.7.4
+#https://projecteuler.net/problem=15
+"""
+There is a deliberate error in the code. 
+Do you understand Python for a long time to find her
+""" 
+from my_function import route_table
+print(id(route_table(20)))
