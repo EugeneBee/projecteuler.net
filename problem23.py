b301c0d1254cb2f85f1aae33e25d1213b078e3f6
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-08-27 20:56:40 +0500

    Create problem23.py

---
+++ problem23.py
@@ -0,0 +1,14 @@
+#Python 3.7.4
+#https://projecteuler.net/problem=1
+"""
+There is a deliberate logical error in the code. 
+Do you understand Python for a long time to find her.
+"""
+from my_function import sumdivis
+arry = [x for x in range(12,28111) if sumdivis(x) >= x]
+arry_set = set()
+for i in arry:
+    for b in arry:
+        arry_set.add(i+b)
+tmp_arry = set([z for z in range(1, 28124)]) - arry_set
+print(sum(tmp_arry))
