76062507cccdae554ad4a3eeb1872b38d89aa13c
EugeneBee <41923643+EugeneBee@users.noreply.github.com>
2019-09-01 10:12:47 +0500

    Create problem34.py

---
+++ problem34.py
@@ -0,0 +1,13 @@
+#Python 3.7.x
+#https://projecteuler.net/problem=1
+"""
+There is a deliberate logical error in the code. 
+Do you understand Python for a long time to find her.
+"""
+from my_function import summ_factorial_num
+
+summ = 0
+for x in range(3,40999):
+	if x != summ_factorial_num(x):
+		summ += x
+print('Result of the problem 34:', summ)
